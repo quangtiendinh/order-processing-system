2889a4c4628290b178915425d22372b6e01198b8
tiendinh0907 <dinhquangtien0169@gmail.com>
2024-07-23 14:38:50 +0700

    Init project

---
+++ src/main/java/com/ops/OrderProcessingSytemApplication.java
@@ -0,0 +1,13 @@
+package com.ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingSytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcessingSytemApplication.class, args);
+	}
+
+}
